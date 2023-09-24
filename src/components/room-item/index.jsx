e6aefd76186c698e496227df0de1dde90e322d90
nishigechuizi <1446307400@qq.com>
2023-09-24 22:57:24 +0800

    轮播图小圆点指示器大致完成

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -1,19 +1,30 @@
 import PropTypes from 'prop-types'
-import React, { memo, useRef } from 'react'
+import React, { memo, useRef,useState } from 'react'
 import {ItemWrapper} from "./style"
 import {Rating} from '@mui/material';
 //轮播图
 import { Carousel } from 'antd';
 import IconArrowLeft from '@/assets/svg/icon-arrow-left';
 import IconArrowRight from '@/assets/svg/icon-arrow-right';
+import Indicator from '@/base-ui/indicator';
+import classNames from 'classnames';
 
 const RoomItem = memo((props) => {
     const {itemData,itemWidth="25%"} = props
     const silderRef = useRef()
+    const [selectIndex,setSelectIndex] = useState(0)
+
     /**事件处理逻辑 */
     function controlClickHandle(isRight = true){
       // 上一个 下一个
       isRight ? silderRef.current.next() : silderRef.current.prev()
+
+      //最新的索引
+      let newIndex = isRight ? selectIndex +1 : selectIndex -1
+      const length = itemData.picture_urls.length
+      if(newIndex < 0)  newIndex = length-1
+      if(newIndex > length-1) newIndex =0
+      setSelectIndex(newIndex)
     }
   return (
     <ItemWrapper 
@@ -31,6 +42,19 @@ const RoomItem = memo((props) => {
             <div className='btn right' onClick={e => controlClickHandle(true)}>
               <IconArrowRight width="30" height="30" ></IconArrowRight>
             </div>
+          </div>
+          <div className='indicator'>
+            <Indicator selectIndex={selectIndex}>
+              {
+                itemData?.picture_urls?.map((item,index) => {
+                  return (
+                    <div className='item' key={item}>
+                      <span className={classNames('dot',{active:selectIndex === index})} ></span>
+                    </div>
+                  )
+                })
+              }
+            </Indicator>
           </div>
           <Carousel dots={false} ref={silderRef}>
             {
--- src/components/room-item/style.js
+++ src/components/room-item/style.js
@@ -61,6 +61,35 @@ export const ItemWrapper = styled.div`
                 }
             }
         }
+
+        .indicator{
+            position: absolute;
+            z-index: 9;
+            bottom: 10px;
+            left: 0;
+            right: 0;
+            margin: 0 auto;
+            width: 30%;
+            .item {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 20%;
+
+                .dot{
+                    width: 6px;
+                    height: 6px;
+                    background-color: #fff;
+                    border-radius: 50%;
+
+                    &.active {
+                        width: 8px;
+                        height: 8px;
+                        background-color: azure;
+                    }
+                }
+            }
+        }
     }
     .desc {
         margin: 10px 0 5px;
