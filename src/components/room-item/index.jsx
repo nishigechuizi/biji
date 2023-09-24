43d0fe894070fbf13f71e99e9fd60bb0f002b40f
nishigechuizi <1446307400@qq.com>
2023-09-24 19:42:00 +0800

    轮播图的实现

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -1,18 +1,48 @@
 import PropTypes from 'prop-types'
-import React, { memo } from 'react'
+import React, { memo, useRef } from 'react'
 import {ItemWrapper} from "./style"
 import {Rating} from '@mui/material';
+//轮播图
+import { Carousel } from 'antd';
+import IconArrowLeft from '@/assets/svg/icon-arrow-left';
+import IconArrowRight from '@/assets/svg/icon-arrow-right';
 
 const RoomItem = memo((props) => {
     const {itemData,itemWidth="25%"} = props
-
+    const silderRef = useRef()
+    /**事件处理逻辑 */
+    function controlClickHandle(isRight = true){
+      // 上一个 下一个
+      isRight ? silderRef.current.next() : silderRef.current.prev()
+    }
   return (
     <ItemWrapper 
       $verifyColor={itemData?.verify_info?.text_color||"#39576a"}
       itemWidth={itemWidth}>
       <div className='inner'>
-        <div className='cover'>
+        {/* <div className='cover'>
           <img src={itemData.picture_url} alt=''></img>
+        </div> */}
+        <div className='slider'>
+          <div className='control'>
+            <div className='btn left' onClick={e => controlClickHandle(false)}>
+              <IconArrowLeft width="30" height="30" ></IconArrowLeft>
+            </div>
+            <div className='btn right' onClick={e => controlClickHandle(true)}>
+              <IconArrowRight width="30" height="30" ></IconArrowRight>
+            </div>
+          </div>
+          <Carousel dots={false} ref={silderRef}>
+            {
+              itemData?.picture_urls?.map((item,index) => {
+                return (
+                  <div className='cover' key={index}>
+                    <img src={item} alt=''></img>
+                  </div>
+                )
+              })
+            }
+          </Carousel>
         </div>
         <div className='desc'>
           {itemData.verify_info.messages.join(".")}
--- src/components/room-item/style.js
+++ src/components/room-item/style.js
@@ -27,6 +27,41 @@ export const ItemWrapper = styled.div`
             object-fit: cover;
         }
     }
+
+    .slider {
+        position: relative;
+        cursor: pointer;
+
+        &:hover {
+            .control {
+                display: flex;
+            }
+        }
+        .control {
+            position: absolute;
+            z-index: 1;
+            left: 0;
+            right: 0;
+            top: 0;
+            display: none;
+            justify-content: space-between;
+            bottom: 0;
+            color: #fff;
+
+            .btn {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 83px;
+                height: 100%;
+                background: linear-gradient(to left,transparent 0%,rgba(0,0,0,0.25) 100%);
+
+                &.right {
+                    background: linear-gradient(to right,transparent 0%,rgba(0,0,0,0.25) 100%); 
+                }
+            }
+        }
+    }
     .desc {
         margin: 10px 0 5px;
         font-size: 12px;
